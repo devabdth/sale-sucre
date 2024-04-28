07d628139c1c6ad81381e1b5f6e0050f0ff56047
devabdth <a.tharwat@cubersio.com>
2024-04-28 14:43:51 +0300

    Consts Init

---
+++ prefs/Consts.ts
@@ -0,0 +1,30 @@
+export const Tabs= [
+    {
+        label: "Home",
+        href: "/",
+    },
+    {
+        label: "Menu",
+        href: "/menu/",
+    },
+    {
+        label: "Hot Offers",
+        href: "/hotOffers/",
+    },
+    {
+        label: "Categories",
+        href: "/categories/",
+    },
+    {
+        label: "Proudcts",
+        href: "/products/",
+    },
+    {
+        label: "About",
+        href: "/about/",
+    },
+    {
+        label: "Contact",
+        href: "/contact/",
+    },
+];
