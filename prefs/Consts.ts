1a392062392b11002fcf841cd99a553396fa5e39
devabdth <a.tharwat@cubersio.com>
2024-05-02 01:48:48 +0300

    Categories Data

--- prefs/Consts.ts
+++ prefs/Consts.ts
@@ -28,3 +28,80 @@ export const Tabs= [
         href: "/contact/",
     },
 ];
+
+export const Services= [
+    {
+        label: "Cakes",
+        description: "Layers of Bliss, One Slice at a Time.",
+        image: "/cakes.webp",
+        products: 16
+    },
+    {
+       label: "Bowls",
+        description: "Wholesome Bowls: Nourish Your Soul.",
+        image: "/bowls.webp",
+        products: 8
+    },
+    {
+        label: "Gateaux",
+        description: "Elegance in Every Decadent Bite.",
+        image: "/gateaux.webp",
+        products: 64
+    },
+    {
+        label: "Soiree",
+        description: "Celebrate Flavor: Our Soiree Collection.",
+        image: "/soiree.webp",
+        products: 12
+    },
+    {
+        label: "Pudding",
+        description: "Creamy Comforts: Dive into Pudding.",
+        image: "/pudding.webp",
+        products: 9
+    },
+    {
+        label: "Les Varrines",
+        description: "Freh Delights: Les Varrines Unleashed.",
+        image: "/les-verrines.webp",
+        products: 16
+    },
+    {
+        label: "Sables",
+        description: "Buttery Crumbles: Savor Our Sables.",
+        image: "/sables.webp",
+        products: 32
+    },
+    {
+        label: "Savory Bites",
+        description: "Small Wonders: Our Savory Bites.",
+        image: "/savoury-bites.webp",
+        products: 28
+    },
+    {
+        label: "Oriental Sweets",
+        description: "Exotic Temptations: Oriental Sweets Await.",
+        image: "/oriental-sweets.webp",
+        products: 16
+    },
+    {
+        label: "Bakery",
+        description: "From Our Ovens to Your Heart.",
+        image: "/bakery.webp",
+        products: 16
+    },
+    {
+        label: "Kahk",
+        description: "Tradition Wrapped in Kahk.",
+        image: "/kahk.webp",
+        products: 4
+    },
+    {
+        label: "Chocolates",
+        description: "Indulge: Our Divine Chocolate Creations.",
+        image: "/choclates.webp",
+        products: 72
+    },
+];
+
+
