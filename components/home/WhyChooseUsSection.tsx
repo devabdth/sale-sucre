ca7f03b8192c0510e196c1c9e444a804698d2489
devabdth <a.tharwat@cubersio.com>
2024-05-02 09:26:41 +0300

    Why Choose Us Section - Small Devices

--- components/home/WhyChooseUsSection.tsx
+++ components/home/WhyChooseUsSection.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import Image from "next/image";
 
 const WhyChooseUsSection= () => {
-    return <section className="w-full h-[45vw] flex flex-col items-center justify-between gap-8 bg-variant py-12">
-      <h2 className="text-6xl text-primary font-extrabold px-72 text-center">
+    return <section className="w-full lg:h-[45vw] md:h-[70vw] h-[85vw] flex flex-col items-center justify-between gap-8 bg-variant py-12">
+      <h2 className="lg:text-6xl md:text-3xl text-2xl text-primary font-extrabold lg:px-72 px-2 text-center">
         Good Things Happen to
         <br />
-        <span className="text-secondary text-[3.25rem]">
+        <span className="text-secondary lg:text-[3.25rem] md:text-4xl text-3xl">
           Those Who Are Interseted!
         </span>
       </h2>
-      <p className="px-[25%] font-normal text-text-primary text-md text-center font-secondary">
+      <p className="lg:px-[25%] px-4 font-normal text-text-primary text-sm text-center font-secondary">
         From Humble Beginnings to Sweet Success: Celebrating Our Journey in Every Bite, Cherishing Memories in Every Flavor, Since 1999.
         </p>
-      <div className="w-full min-h-[25vw] flex flex-row items-center justify-between gap-16">
+      <div className="w-full lg:min-h-[25vw] min-h-[40vw] flex flex-row items-center justify-between lg:gap-16 gap-4">
         <div className="w-full rounded-r-lg h-full overflow-hidden">
           <Image
             src="/store.jpg"
