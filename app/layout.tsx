f5963578a852e74f7f258c78a6f8b20014323fbb
devabdth <a.tharwat@cubersio.com>
2024-05-02 17:31:55 +0300

    Fav Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,19 @@
 import { DefaultFooter, DefaultHeader } from "@/components/globals";
 import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TreGurus - Sale Sucre",
+  icons: [
+    {
+        rel: "icon",
+        type: "image/ico",
+        sizes: "64x64",
+        url: "./favicon.ico"
+    },
+  ],
+  description: "Solid Draft Presented by TreGurus",
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-        <body>
+      <body>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
             <DefaultHeader />
             {children}
             <DefaultFooter />
